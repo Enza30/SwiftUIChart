4c24e8d5bc87ad5916c6872621d1e8dee5d2fb14
Farendza <farendza.muda@gmail.com>
2021-11-10 09:04:50 +0700

    Initial Commit

---
+++ SwiftUIChart/SwiftUIChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartApp.swift
+//  SwiftUIChart
+//
+//  Created by Farendza Muda on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
